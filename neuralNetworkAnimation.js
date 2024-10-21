fa26b452591170eb381a49c58e7cbb49cd738b2e
OwenKelley <114434876+OwenKelley@users.noreply.github.com>
2024-10-21 09:06:32 -0400

    Update neuralNetworkAnimation.js

--- neuralNetworkAnimation.js
+++ neuralNetworkAnimation.js
@@ -19,12 +19,10 @@ class Node {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = (Math.random() % 0.3) * 2; // Size of the node
+    this.radius = (Math.random() % 0.7) * 2 + Math.random(); // Size of the node (3)
     this.vx = (Math.random() - 0.5) * .7; // Horizontal velocity
-    this.vy = (Math.random() - 0.5) * .7; // Vertical velocity - 0.5
+    this.vy = (Math.random() - 0.5) * .7; // Vertical velocity
   }
-//Math.random() % .3
-
 
   // Update the node's position
   update() {
